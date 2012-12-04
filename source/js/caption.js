f8df9e190f93bae0a24872a9a728ec20ce6b693b
Tommy Chen <tommy351@gmail.com>
2012-12-04 23:40:24 +0800

    Moved caption to a separated file

---
+++ source/js/caption.js
@@ -0,0 +1,15 @@
+(function($){
+  $('.entry').each(function(i){
+    $(this).find('img').each(function(){
+      var alt = this.alt;
+
+      if (alt){
+        $(this).after('<span class="caption">' + alt + '</span>');
+      }
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" rel="gallery' + i + '" />');
+    });
+  });
+
+  $('.fancybox').fancybox();
+})(jQuery);
